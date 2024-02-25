da11093a6fd0dc7539df14b38ea11ec661daa2a1
diego <diegodataminer@gmail.com>
2024-02-25 11:13:54 -0300

    add slider

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ st.write('''this is out firts web app
          
          ''')
 st.write(df)
+
+st.line_chart(df)
+
+st.area_chart(df)
+
+myslider = st.slider('celsius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
