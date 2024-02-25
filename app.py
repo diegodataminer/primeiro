ffce048b16632f6fc97cc9a6f5d7959881486b50
diego <diegodataminer@gmail.com>
2024-02-25 10:25:06 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('our first streamlit app')
+st.subheader('introducing streamlit')
+st.write('''this is out firts web app
+         enjoy!
+         
+         
+         ''')
